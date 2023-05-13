015334761fb1332023ecf4f7bbe5b5e74112ed79
sumbatx15 <sumbat15@gmail.com>
2023-05-14 00:48:41 +0300

    chore: added export to bg

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from "./components/Diagram/WrappedDiagram";
 export * from "./components/Diagram/edge";
 export * from "./components/Diagram/handle";
+export * from "./components/background/Background";
 export * from "./store";
 export * from "./App.css";
 export const test = "test";
